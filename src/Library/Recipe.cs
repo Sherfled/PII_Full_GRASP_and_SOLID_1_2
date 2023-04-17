f27e162e37bf456d543cffe62f36e687eccbd228
Sherfled <diego.zucca@correo.ucu.edu.uy>
2023-04-17 11:35:17 -0300

    Cambios

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -25,6 +25,19 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
+
+        public double GetProductionCost()
+        {
+            int ins_cost = 0;
+            int equip_cost = 0;
+            foreach (Step step in this.steps)
+            {
+                ins_cost = ins_cost + step.Input.GetUnitCost;  // ins_cost = ins_cost + step.Quantity es lo mismo?  // Primera parte
+                equip_cost = equip_cost + step.Equipment.GetHourlyCost * step.GetTime; // Segunda parte
+            }
+            return ins_cost + equip_cost;
+        }
+
         public void PrintRecipe()
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
@@ -33,6 +46,10 @@ namespace Full_GRASP_And_SOLID.Library
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"El costo unitario es {GetProductionCost()}"); // Tercera parte
         }
     }
 }
